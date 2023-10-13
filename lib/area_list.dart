8f8eadacaf4e12fd783cdeb77cc5f15385173da3
Sohag-84 <ihsohag84@gmail.com>
2023-10-13 16:29:11 +0600

    item search is dropdown menu

---
+++ lib/area_list.dart
@@ -0,0 +1,17 @@
+List<String> areaList = [
+  "Kishoreganj",
+  "Sylhet",
+  "Dhaka",
+  "Netrokona",
+  "Jamalpur",
+  "Cumilla",
+  "Dinajpur",
+  "Faridpur",
+  "Mymensingh",
+  "Noakhali",
+  "Bogra",
+  "Tangail",
+  "Pabna",
+  "Gopalganj",
+  "Rajshahi"
+];
